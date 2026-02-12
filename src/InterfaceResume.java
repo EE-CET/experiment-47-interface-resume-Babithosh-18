f8f494a9b06a3b91818cc8c1ce26561796c3ea7e
Babithosh <ellu250071@cet.ac.in>
2026-02-13 00:45:16 +0530

    interface resume code added

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 interface Resume {
     // TODO: Declare method void biodata();
+
+	 void displayInfo();
 }
 
 class Teacher implements Resume {
     // Attributes
-   
-
     // TODO: Implement biodata() method to print details in the required format
+
+	    private String name;
+    private String subject;
+    private int experienceYears;
+
+    Teacher(String name, String subject, int experienceYears) {
+        this.name = name;
+        this.subject = subject;
+        this.experienceYears = experienceYears;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Qualification: " + subject);
+System.out.println("Experience: " + experienceYears + " years");
+    }
 }
 
 public class InterfaceResume {
@@ -16,7 +32,17 @@ public class InterfaceResume {
         
         // TODO: Read input (Name, Qualification, Experience)
         // Assign to teacher object attributes
-        
         // TODO: Call teacher.biodata()
     
+	public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+      String name = input.nextLine();
+      String subject = input.nextLine();
+      int experienceYears = input.nextInt();
+      Resume teacherResume = new Teacher(name, subject, experienceYears);
+      teacherResume.displayInfo();
+      input.close(); 
+
+   }  
 }
